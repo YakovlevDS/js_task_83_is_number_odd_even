dea176cdc1f2dafc221ccb53611740c28b5cdec6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-23 20:24:36 +0200

    added num is even

---
+++ app.js
@@ -0,0 +1,13 @@
+// ? Task:Check if a number is even or odd
+
+
+// Solution 1
+const isEven = num => num % 2 === 0;
+
+console.log(isEven(2)); 
+console.log(isEven(1232)); 
+console.log(isEven(25)); 
+
+
+
+// ! Explanation: 
